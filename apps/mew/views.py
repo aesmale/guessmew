09fff00f7054cd7a4469561ba064502068daf5b3
Amber Esmale <ismila2@gmail.com>
2016-12-21 10:57:46 -0800

    view

--- apps/mew/views.py
+++ apps/mew/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render
-from .models import Cat, Player, Board
 from random import randint
 from .models import Player, Cat, Board
 import itertools
 
 
 # Create your views here.
-
 def newgame(request):
     request.session.flush()
     Player.objects.all().delete()
@@ -61,7 +59,6 @@ def newgame(request):
     request.session['opponent_id'] = this_opponent.id
     request.session['player_board_id'] = player_board.id
     request.session['opponent_board_id'] = opponent_board.id
-
     player_cats = Board.objects.filter(id  = request.session['player_board_id']),
 
     context = {
@@ -85,4 +82,3 @@ def game(request):
     }
 
     return render(request, "mew/index.html", context)
-
