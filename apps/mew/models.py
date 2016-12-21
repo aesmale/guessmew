99fb324701e19e466ef1d68ae5c2ab2145b3eccc
Amber Esmale <ismila2@gmail.com>
2016-12-20 17:20:42 -0800

    adding models and views

--- apps/mew/models.py
+++ apps/mew/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Cat(models.Model):
+    name = models.Charfield(max_length = 100)
+    color = models.CharField(max_length = 100)
+    fur = models.Charfield(max_length = 100)
+    glasses = models.BooleanField(default = False)
+    scarf = models.BooleanField(default = False)
+    hat = models.BooleanField(default = False)
+
+class Player(models.Model):
+    chosencat = models.ForeignKey(Cat)
+
+class Board(models.Model):
+    player = models.ForeignKey(Player)
+    cats = models.ManyToManyField(Cat)
--- apps/mew/views.py
+++ apps/mew/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
+from random import randint
 
 # Create your views here.
 def index(request):
     return render(request, "mew/index.html")
+
+def newgame(request):
+    player_choice = Cat.objects.get(id = request.POST['cat'])
+    opponent_choice = Cat.objects.get(id = randint(1,25))
+
+
+    #CREATING PLAYER AND OPPONENT
+    this_player = Player.objects.create(chosencat = player_choice)
+    this_opponent = Player.objects.create(chosencat = opponent_choice)
+
+
+    #CREATING GAMEBOARD FOR PLAYER AND OPPONENT
+    player_board = Board.objects.create(player = request.session['player'])
+    opponent_board = Board.objects.create(player = request.session['opponent'])
+
+
+    #ADDING ALL CATS TO EACH GAME BOARD
+    all_cats = Cats.objects.all()
+    player_board.cats.add(all_cats)
+    opponent_board.cats.add(all_cats)
+
+    #SETTING SESSION VARIABLES FOR PLAYER, OPPONENT, AND RESPECTIVE GAMEBOARDS
+    request.session['player_id'] = this_player.id
+    request.session['opponent_id'] = this_opponent.id
+    request.session['player_board_id'] = player_board.id
+    request.session['opponent_board_id'] = opponent_board.id
