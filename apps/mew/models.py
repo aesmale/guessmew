ff8511e23c4971095169c11debcc215720a5bff1
Amber Esmale <ismila2@gmail.com>
2016-12-21 16:27:32 -0800

    migrations

--- apps/mew/models.py
+++ apps/mew/models.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Board(models.Model):
+    player = models.ForeignKey('Player')
+
 class Cat(models.Model):
     name = models.CharField(max_length = 100)
     color = models.CharField(max_length = 100)
@@ -10,10 +13,7 @@ class Cat(models.Model):
     glasses = models.BooleanField(default = False)
     scarf = models.BooleanField(default = False)
     hat = models.BooleanField(default = False)
+    board = models.ManyToManyField(Board)
 
 class Player(models.Model):
     chosencat = models.ForeignKey(Cat)
-
-class Board(models.Model):
-    player = models.ForeignKey(Player)
-    cats = models.ManyToManyField(Cat)
